8d03deba86690686ea85733d532f3506d6af123a
Jim Yang <jyang_123@hotmail.com>
2018-09-28 12:59:06 -0400

    added login and user components for testing

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
-import React from 'react';
+import { createStackNavigator } from 'react-navigation';
+import User from './Components/UserScreen';
+import LoginScreen from './Components/LoginScreen';
+import UserScreen from './Components/UserScreen';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const App = createStackNavigator({
+  Login: { screen: LoginScreen },
+  User: { screen: UserScreen }
 });
+
+export default App;
---
+++ secrets.js
@@ -0,0 +1,4 @@
+process.env.GOOGLE_CLIENT_ID =
+  '43273242255-760i8ukv0pm8t0qb51acpc2p2nsklh5c.apps.googleusercontent.com';
+process.env.GOOGLE_CLIENT_SECRET = 'vieU9Ykk47jpByYJi-pjUUFx';
+process.env.GOOGLE_CALLBACK = '/auth/google/callback';
