eac37ad3181576255b61ddc5b480e065aba1c692
Devam M <devam493@gmail.com>
2018-10-10 09:28:40 -0400

    activity page styling

--- Components/ActivitiesScreen.js
+++ Components/ActivitiesScreen.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import { fetchActivities } from '../store/trip';
-import { StyleSheet, Text, SafeAreaView, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, View, TouchableOpacity, Image } from 'react-native';
 import Navbar from './Navbar';
 import AppLink from 'react-native-app-link';
+const openIcon = require('../assets/images/open.png')
 
 
 class Activities extends Component {
@@ -24,24 +25,29 @@ class Activities extends Component {
 
   render() {
     return (
-      <View>
+      <View >
         <Navbar />
-          <View >
-            <Text>Activities List:</Text>
-              {!this.props.trip.activities.length
-                ? null
-                : this.props.trip.activities.map(activity => {
-                    return (
-                      <TouchableOpacity
-                        onPress={() => this.handlePress(activity.location)}
-                        key={activity.id}
-                      >
-                        <Text>{activity.name}</Text>
-                      </TouchableOpacity>
-                    );
-                })
-              }
-          </View>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Activities List:</Text>
+        </View>
+        <View styles={styles.container}>
+        {!this.props.trip.activities.length
+            ? null
+            : this.props.trip.activities.map(activity => {
+              return (       
+                <TouchableOpacity
+                  onPress={() => this.handlePress(activity.location)}
+                  key={activity.id}
+                  style={styles.activity}
+                >
+                  <Text style={{fontSize: 20}}>{activity.name}</Text>
+                  <Image source={openIcon} style={{height: 20, width: 20}} />
+                </TouchableOpacity>
+              );
+            })
+          }
+        
+        </View>
       </View>
     );
   }
@@ -57,5 +63,30 @@ const mapDispatchToProps = dispatch => {
     fetchActivities: id => dispatch(fetchActivities(id))
   };
 };
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#00adef'
+  },
+  activity: {
+    marginTop: 20,
+    marginBottom: 20,
+    backgroundColor: 'red',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '70%'
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 30,
+    textAlign: 'center'
+  },
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Activities);
