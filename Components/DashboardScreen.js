1d305428ddf95b22619cbead92cdc99566186199
mbungeroth <mbungeroth@gmail.com>
2018-10-13 23:55:56 -0400

    took out persistent login

--- Components/DashboardScreen.js
+++ Components/DashboardScreen.js
@@ -2,30 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import AllTrips from './AllTrips';
 import { fetchTrips } from '../store/trip';
-import { StyleSheet, Text, ScrollView, View, AsyncStorage } from 'react-native';
+import { StyleSheet, Text, ScrollView, View, } from 'react-native';
 import Navbar from './Navbar';
 
-const retrieveData = async (key) => {
-  try {
-    const value = await AsyncStorage.getItem(key);
-    if (value !== null) {
-      return value;
-    }
-  } catch (error) {
-    console.log(error)
-  }
-}
-
 class Dashboard extends Component {
-  async componentDidMount() {
-    if (this.props.navigation.state.params.userExists) {
-      const offlineUserString = await retrieveData('user');
-      const offlineUser = JSON.parse(offlineUserString);
-      this.props.fetchTrips(offlineUser["id"]);
-    } else {
-      this.props.fetchTrips(this.props.user.id);
+  componentDidMount() {
+    this.props.fetchTrips(this.props.user.id);
     }
-  }
+
   render() {
     return (
       <View>
